3add1c0cbd41d3d9227d76a9139848f9b6d41897
Maciejfiedler <54156166+Maciejfiedler@users.noreply.github.com>
2020-05-22 22:04:59 +0200

    Update web.py

--- web.py
+++ web.py
@@ -12,28 +12,35 @@ class App(object):
     def __init__(self):
         self.root = tk.Tk()
         self.root.title("Autostart Browser Windows")
-        self.root.geometry('600x300')
-        self.root.resizable(False, False)
-        frm = ttk.Frame(self.root)
-        frm.pack(expand=False, fill='both')
-        scrollbar = ttk.Scrollbar(frm)
-        scrollbar.pack(side=RIGHT, fill=Y)
-        urlinput = ttk.Entry(frm, width=80)
-        urlinput.grid(row=0, column=1, sticky=W, padx=10)
+        self.root.resizable(True, True)
+        frame = ttk.Frame(self.root)
+        frame.pack(expand=False, fill='both')
+        # Scrollbar
+        scrollbar = ttk.Scrollbar(frame)
+        scrollbar.grid(row=1, column=2, sticky="nse")
+        # Listbox
+        urllist = tk.Listbox(
+            frame, yscrollcommand=scrollbar.set, width=90, height=20)
+        urllist.grid(pady=10, padx=10)
+        # Entry
+        urlinput = ttk.Entry(frame, width=50)
+        urlinput.grid(row=0, column=1, sticky='n', pady=10)
+        # Pack
+        urllist.grid(row=1, column=1)
+        scrollbar.config(command=urllist.yview)
+        # Button
         urlbutton = ttk.Button(
-            frm, text='Add', command=lambda: self.addurl(urlinput, frm, scrollbar))
-        urlbutton.grid(row=0, column=2, sticky=W)
+            frame, text='Add', command=lambda: self.addurl(urlinput, frame, scrollbar, urllist))
+        urlbutton.grid(row=0, column=1, sticky='ne', pady=10, padx=20)
 
-    def addurl(self, input, frame, scrollbar):
+    def addurl(self, entry, frame, scrollbar, listbox):
         # Add URL
-        self.urls.insert(0, input.get())
+        self.urls.insert(0, entry.get())
         print(self.urls)
         # Show URL
-        listurl = Listbox(frame, yscrollcommand=scrollbar.set)
         for url in self.urls:
-            listurl.insert(END, url)
-        listurl.pack()
-        scrollbar.config(command=listurl.yview)
+            listbox.insert(END, url)
+            entry.delete(0, END)
 
 
 app = App()
